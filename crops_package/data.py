3cc893113567944774a71b9b07d01d33b192e12c
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2024-03-05 15:52:37 +0100

    Get list of pictures to delete

--- crops_package/data.py
+++ crops_package/data.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 import os
 
-tf.keras.preprocessing.image_dataset_from_directory(
-    directory=os.environ.get("LOCAL_PATH"),
-    labels='inferred',
-    image_size=(224,224),
-    # class_names=
-    label_mode="categorical",
-    batch_size=32
-)
+def preprocessor():
+    tf.keras.preprocessing.image_dataset_from_directory(
+        directory=os.environ.get("LOCAL_PATH"),
+        labels='inferred',
+        image_size=(224,224),
+        # class_names=
+        label_mode="categorical",
+        batch_size=32
+    )
