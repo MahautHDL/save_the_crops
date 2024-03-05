54c5f23ed2f8e10dc61f8d9ef17a8c5218ac4cbf
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2024-03-05 14:12:54 +0100

    Voorlopig

---
+++ crops_package/data.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import os
+
+tf.keras.preprocessing.image_dataset_from_directory(
+    directory=os.environ.get("LOCAL_PATH"),
+    labels='inferred',
+    image_size=(224,224),
+    # class_names=
+    label_mode="categorical",
+    batch_size=32
+)
