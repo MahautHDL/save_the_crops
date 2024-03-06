52b4a2ee4023f99c7f40e1373b37ec7f77701e7e
Mahaut de Lesquen <mahaut.de.lesquen@gmail.com>
2024-03-06 17:37:47 +0100

    new function in data to preprocess the test or image / and in the v3, made the model.evaluate and model.predict work

--- crops_package/data.py
+++ crops_package/data.py
@@ -1,13 +1,15 @@
 import tensorflow as tf
 import pandas as pd
 import os
+import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
 from sklearn.model_selection import train_test_split
 
-def split_data(df):
-    train_df, test_df = train_test_split(df, test_size=0.15, random_state=42)
+def split_data(df, size_to_throw=0.9):
+    resized_df, to_throw = train_test_split(df, test_size=size_to_throw, random_state=42)
+    train_df, test_df = train_test_split(resized_df, test_size=0.15, random_state=42)
     train_df, val_df = train_test_split(train_df, test_size=0.15, random_state=42)
 
     return train_df, val_df, test_df
@@ -62,3 +64,11 @@ def preprocessor_test(df):
         shuffle=True
     )
     return generator
+
+
+def prepocessor_img(file_path):
+    img = tf.io.read_file(file_path)
+    img = tf.image.decode_image(img, channels=3)  # convert to tensor and define 3 colors
+    img = tf.image.resize(img, [224, 224])
+    img = np.expand_dims(img, axis=0)
+    return img
