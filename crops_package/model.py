f62bdc2460d16a2fffafb79cefcb6b820542b9bc
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2024-03-07 13:53:57 +0100

    earlystopping

--- crops_package/model.py
+++ crops_package/model.py
@@ -4,28 +4,28 @@ import os
 
 from tensorflow.keras.callbacks import EarlyStopping
 
-
 # Create tensorboard callback (functionized because need to create a new one for each model)
 def create_tensorboard_callback(dir_name, experiment_name):
-  log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  log_dir = dir_name + "/" + experiment_name + "-" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
   tensorboard_callback = tf.keras.callbacks.TensorBoard(
       log_dir=log_dir
   )
   print(f"Saving TensorBoard log files to: {log_dir}")
   return tensorboard_callback
 
-# fitting a model
+import tensorflow as tf
+import datetime
+import os
 
-def fit_model(model, X, y, name_callback, split=0.3, epochs=100):
+def fit_model(model, X, validation_data, model_name, split=0.3, epochs=100):
     es = EarlyStopping(patience = 5, verbose = 2)
 
     history = model.fit(X,
-                        y,
-                        validation_split = split,
-                        callbacks = [es],
+                        validation_data = validation_data,
+                        # validation_split = split,
                         epochs = epochs,
-                        callbacks=[create_tensorboard_callback(dir_name=os.environ.get("CALLBACK_PATH"), # save experiment logs here
-                                                                         experiment_name=name_callback)] # name of log files
+                        callbacks=[es, create_tensorboard_callback(dir_name=os.environ.get("CALLBACK_PATH"), # save experiment logs here
+                                                                         experiment_name=model_name)] # name of log files
     )
 
     return history
