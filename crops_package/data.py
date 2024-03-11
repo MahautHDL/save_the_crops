e02e0be8bf6755e4270dc242f099ef208ea1b596
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2024-03-11 10:40:03 +0100

    small_changes

--- crops_package/data.py
+++ crops_package/data.py
@@ -5,14 +5,61 @@ import os
 import numpy as np
 import requests
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+
+def check_for_errors_and_create_excel():
+    """
+    Will check if pictures are not corrupted.
+    Then it will create and save a csv with the filename and class of the non-corrupted pictures.
+    Output is a dataframe from this csv.
+    """
+    data_link = os.environ.get("LOCAL_PATH")
+
+    data = pd.DataFrame(columns=["filename", "class"])
+
+    crops = os.listdir(data_link)
+
+    for i in range(0,len(crops)):
+        plant_disease_name = crops[i]
+        os.chdir(f'{data_link}{plant_disease_name}')
+
+        filenames = os.listdir(f"{data_link}{plant_disease_name}")
+
+        for index, filename in enumerate(filenames):
+            if filename.endswith(".jpg"):
+
+                try:
+                    plt.imread(filename)
+                    data.loc[len(data)] = [f'{plant_disease_name}/{filename}', plant_disease_name]
+
+                except:
+                    pass
+            else:
+                pass
+
+    data.to_csv(f'{os.environ.get("DATA_PATH")}data.csv', index=False)
+
+    return data
 
 
 def split_data(df):
-    train_df, test_df = train_test_split(df, test_size=0.15, random_state=42, stratify=y)
-    train_df, val_df = train_test_split(train_df, test_size=0.15, random_state=42, stratify=y)
+    train_df, test_df = train_test_split(df, test_size=0.15, random_state=42, stratify=df['class'])
+    train_df, val_df = train_test_split(train_df, test_size=0.15, random_state=42, stratify=train_df['class'])
     return train_df, val_df, test_df
+
+
+def split_and_reduce_data(df, size_to_throw=0.001):
+    """
+    Will split data into train_df, val_df and test_df.
+    size_to_throw is the amount of data not to use (between 0.001 and 1)
+    """
+    resized_df, to_throw = train_test_split(df, test_size=size_to_throw, random_state=42, stratify=df['class'])
+    train_df, test_df = train_test_split(resized_df, test_size=0.15, random_state=42, stratify=resized_df['class'])
+    train_df, val_df = train_test_split(train_df, test_size=0.15, random_state=42, stratify=train_df['class'])
+    return train_df, val_df, test_df
+
 
 
 def preprocessor_df(df):
@@ -30,7 +77,7 @@ def preprocessor_df(df):
         df,
         x_col='filename',
         y_col='class',
-        target_size=(224, 224),
+        target_size=(128, 128),
         batch_size=32,
         class_mode='categorical',
         shuffle=True
@@ -46,7 +93,7 @@ def preprocessor_test(df):
         df,
         x_col='filename',
         y_col='class',
-        target_size=(224, 224),
+        target_size=(128, 128),
         batch_size=32,
         class_mode='categorical',
         shuffle=True
@@ -63,7 +110,7 @@ def prepocessor_img_from_path(file_path):
         img = tf.io.read_file(file_path)
         img = tf.image.decode_image(img, channels=3)
     # Resizing the image
-    img = tf.image.resize(img, [224, 224])
+    img = tf.image.resize(img, [128, 128])
     # Add one dimension batch
     img = np.expand_dims(img, axis=0)
     return img
@@ -72,7 +119,7 @@ def prepocessor_img_from_path(file_path):
 def prepocessor_img(file):
     img = tf.image.decode_image(file, channels=3)
     # Resizing the image
-    img = tf.image.resize(img, [224, 224])
+    img = tf.image.resize(img, [128, 128])
     # Add one dimension batch
     img = np.expand_dims(img, axis=0)
     return img
--- crops_package/model.py
+++ crops_package/model.py
@@ -17,15 +17,15 @@ import tensorflow as tf
 import datetime
 import os
 
-def fit_model(model, X, validation_data, model_name, split=0.3, epochs=100):
+def fit_and_save_model(model, X, validation_data, model_name, split=0.3, epochs=100):
     es = EarlyStopping(patience = 5, verbose = 2)
 
     history = model.fit(X,
                         validation_data = validation_data,
-                        # validation_split = split,
                         epochs = epochs,
                         callbacks=[es, create_tensorboard_callback(dir_name=os.environ.get("CALLBACK_PATH"), # save experiment logs here
                                                                          experiment_name=model_name)] # name of log files
     )
 
+    model.save(f'./models/{model_name}')
     return history
