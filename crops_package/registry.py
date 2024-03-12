429fa728e0d3b72dd7ffeeeb2d3b79294a5ad69e
Victor De Coster <victor.decoster94@gmail.com>
2024-03-12 11:15:06 +0100

    load mlflow code in registry

--- crops_package/registry.py
+++ crops_package/registry.py
@@ -3,82 +3,40 @@ import os
 import time
 import pickle
 
-#from colorama import Fore, Style
+# from colorama import Fore, Style
 from tensorflow import keras
 
 # from taxifare.params import *
-#import mlflow
-#from mlflow.tracking import MlflowClient
+import mlflow
+from mlflow.tracking import MlflowClient
+from mlflow.models import Model
+from tensorflow import keras
 
-'''def save_model(model: keras.Model = None) -> None:
+
+def load_model(stage="Production") -> keras.Model: #
     """
-    Persist trained model locally on the hard drive at f"{LOCAL_REGISTRY_PATH}/models/{timestamp}.h5"
-    - if MODEL_TARGET='gcs', also persist it in your bucket on GCS at "models/{timestamp}.h5" --> unit 02 only
-    - if MODEL_TARGET='mlflow', also persist it on MLflow instead of GCS (for unit 0703 only) --> unit 03 only
-    """
+    Loads a saved model from MLflow, trying multiple tracking URIs in sequence.
 
-    timestamp = time.strftime("%Y%m%d-%H%M%S")
-
-    # Save model locally
-    model_path = os.path.join(LOCAL_REGISTRY_PATH, f"{timestamp}.h5")
-    model.save(model_path)
-
-    print("✅ Model saved locally")
-
-    mlflow.tensorflow.log_model(
-        model=model,
-        artifact_path="model",
-        registered_model_name=MLFLOW_MODEL_NAME
-        )
-
-    print("✅ Model saved to MLflow")
-
-    return None'''
-
-
-
-
-
-def load_model(stage="Production") -> keras.Model:
-    """
-    Return a saved model:
-    - locally (latest one in alphabetical order)
-    - or from GCS (most recent one) if MODEL_TARGET=='gcs'  --> for unit 02 only
-    - or from MLFLOW (by "stage") if MODEL_TARGET=='mlflow' --> for unit 03 only
-
-    Return None (but do not Raise) if no model is found
-
+    Returns the loaded model or None if no model is found after trying all tracking URIs.
     """
 
+    tracking_uri = os.getenv('MLFLOW_TRACKING_URI')
+    mlflow.set_tracking_uri(tracking_uri)
+    client = MlflowClient()
 
-    local_model_paths = '/Users/thomasverpoort/code/MahautHDL/save_the_crops/models/first_model_07_03'
+    # Get all versions of the model filtered by name
+    model_name = os.getenv('MLFLOW_MODEL_NAME')
 
-    model = keras.models.load_model(local_model_paths)
+    model_versions = client.get_latest_versions(name=model_name, stages=[stage])
+    model_uri = model_versions[0].source
+    model = mlflow.tensorflow.load_model(model_uri=model_uri)
 
     return model
 
 
-#     elif MODEL_TARGET == "mlflow":
-#         print(Fore.BLUE + f"\nLoad [{stage}] model from MLflow..." + Style.RESET_ALL)
 
-#         # Load model from MLflow
-#         model = None
-#         mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-#         client = MlflowClient()
+#mlflow.set_tracking_uri("https://mlflow.lewagon.ai")
 
-#         try:
-#             model_versions = client.get_latest_versions(name=MLFLOW_MODEL_NAME, stages=[stage])
-#             model_uri = model_versions[0].source
+#model_uri = "models:/taxifare_model/Production"
 
-#             assert model_uri is not None
-#         except:
-#             print(f"\n❌ No model found with name {MLFLOW_MODEL_NAME} in stage {stage}")
-
-#             return None
-
-#         model = mlflow.tensorflow.load_model(model_uri=model_uri)
-
-#         print("✅ Model loaded from MLflow")
-#         return model
-#     else:
-#         return None
+#model = mlflow.tensorflow.load_model(model_uri=model_uri)
